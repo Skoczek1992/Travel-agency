ff2271268da8235ff2371eecc576fb65c83935f2
Skoczek1992 <s.skoczylas92@gmail.com>
2019-11-18 20:40:52 +0100

    add order commponents

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,9 +2,18 @@ import React from 'react';
 import {Col, Row} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
+import OrderOption from '../OrderOption/OrderOption.js';
+import Pricing from '../../../data/pricing.json';
 
-const OrderForm = props => (
+const OrderForm =  (props) => (
   <Row>
+    {Pricing.map((option) => (
+      <Col md={4} key={option.id}>
+        <OrderOption {...option}
+          currentValue={props.options[option.id]}
+          setOrderOption={props.setOrderOption} />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
@@ -14,6 +23,7 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
 };
 
 export default OrderForm;
